d15834dc4426056191e526da63e30a8b469b35dc
Hikaru20210918 <hs2m.1022@gmail.com>
2023-02-11 00:10:22 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,15 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+
+    @item = Item.all.order(id: "DESC")
+   
   end
 
   def new
+
     @item = Item.new
+    
   end
 
   def create
@@ -20,8 +25,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:detail, :image, :item_name, :price, :category_id, :states_id, :area_id, :postage_id,
-                                 :sipping_day_id).merge(user_id: current_user.id)
-    # 学習メモ：.merge()内のカラム名は user_id 「id」の記述が必要
+    params.require(:item).permit(:detail, :image, :item_name, :price, :category_id, :states_id, :area_id, :postage_id,:sipping_day_id).merge(user_id: current_user.id)
+    # 学習メモ：.merge()内のカラム名は user_id の「_id」記述が必要
   end
 end
