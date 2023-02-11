029909a484e79cf18a9e7858bdb28a537088dba9
Hikaru20210918 <hs2m.1022@gmail.com>
2023-02-11 21:23:44 +0900

    商品情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users 
   #学習メモ：devise_forは、ユーザー機能に必要な複数のルーティングを一度に生成してくれるdeviseのメソッド
   root to: "items#index"
-  resources :items, only: [:new, :create ,:show]
+  resources :items, only: [:new, :create ,:show, :edit, :update]
 end
